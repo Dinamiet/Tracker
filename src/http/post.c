47cc22ca7b1a4445b52a8ecc8d189522569d26ca
Dinamiet <iceman.sm39@gmail.com>
2025-11-23 07:17:26 +0200

    Encrypted http post

---
+++ src/http/post.c
@@ -0,0 +1,18 @@
+#include "http.h"
+#include "sim868_http.h"
+#include "comms.h"
+
+#include "aes.h"
+
+static uint8_t KEY[] = {};
+static uint8_t IV[] = {};
+
+void HTTP_PostData(char* url, void* data, size_t size)
+{
+	AES aes;
+	AES_Init(&aes, IV, KEY, AES_256);
+
+	size_t encryptedSize = AES_CBC_Encrypt(&aes, data, size, true);
+
+	SIM868_HTTP_Post(atTerm, url, data, encryptedSize);
+}
