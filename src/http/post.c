2b2135f859f64dcc2bb652060b9ae618a442bdee
Dinamiet <iceman.sm39@gmail.com>
2025-11-23 07:51:39 +0200

    Move keys to seperate file

--- src/http/post.c
+++ src/http/post.c
@@ -1,17 +1,20 @@
 #include "aes.h"
 #include "comms.h"
 #include "http.h"
+#include "private.h"
 #include "sim868_http.h"
-
-static uint8_t KEY[] = {};
-static uint8_t IV[] = {};
+#include "secrets.h"
 
 void HTTP_PostData(char* url, void* data, size_t size)
 {
+	if (HTTP_IsBusy())
+		return;
+
 	AES aes;
 	AES_Init(&aes, IV, KEY, AES_256);
 
 	size_t encryptedSize = AES_CBC_Encrypt(&aes, data, size, true);
 
+	HTTP_SetBusy(true);
 	SIM868_HTTP_Post(atTerm, url, data, encryptedSize);
 }
---
+++ src/http/secrets.h
@@ -0,0 +1,9 @@
+#ifndef _SECRETS_H_
+#define _SECRETS_H_
+
+#include <stdint.h>
+
+static uint8_t KEY[] = {};
+static uint8_t IV[] = {};
+
+#endif
