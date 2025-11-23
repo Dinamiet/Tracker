647c645aed449f3d8e302522a72f70db231a1e8d
Dinamiet <iceman.sm39@gmail.com>
2025-11-23 20:06:17 +0200

    Add configuration values

--- include/config.h
+++ include/config.h
@@ -1,11 +1,18 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
+#define CONFIG_DEVICE_NAME ""
+
 #define CONFIG_CONNECTION_ID 1
 #define CONFIG_APN           "internet"
 
 #define CONFIG_GPS_FIX_INTERVAL 10
 
 #define CONFIG_LINKED_PHONE ""
+
+#define CONFIG_LOCATION_POST_URL ""
+
+#define CONFIG_STATIONARY_SPEED_THRESHOLD 5   // m/s
+#define CONFIG_STATIONARY_TIMEOUT         300 // seconds
 
 #endif
