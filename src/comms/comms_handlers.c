5d26135fe64a3e3368e0e0c519f4c70c01da3367
Dinamiet <iceman.sm39@gmail.com>
2025-11-29 09:45:01 +0200

    Power GPS when device starts

--- src/comms/comms_handlers.c
+++ src/comms/comms_handlers.c
@@ -1,3 +1,4 @@
+#include "gps.h"
 #include "private.h"
 #include "sim868_misc.h"
 
@@ -10,6 +11,8 @@ void Ready_Handler(ATTerminal* at, char* param)
 
 	SIM868_Misc_CommandEcho(at, false);
 	printf("Device Ready\n");
+
+	GPS_Enable(true);
 }
 
 void Default_Handler(ATTerminal* at, char* param)
