cd59b031d67551b5db222697f659b10ed63fa2fc
Dinamiet <iceman.sm39@gmail.com>
2025-12-04 16:36:01 +0200

    Timeout busy HTTP request

--- src/http/busy.c
+++ src/http/busy.c
@@ -1,7 +1,33 @@
+#include "comms.h"
+#include "config.h"
+#include "math.h"
 #include "private.h"
+#include "sim868_http.h"
+#include "utilities.h"
 
-static bool ongoingRequest = false;
+static bool     ongoingRequest   = false;
+static uint32_t requestStartTime = 0;
 
-bool HTTP_IsBusy() { return ongoingRequest; }
+bool HTTP_IsBusy()
+{
+	if (ongoingRequest)
+	{
+		uint32_t now = Time_ms();
+		if ((requestStartTime + CONFIG_HTTP_BUSY_TIMEOUT) < now)
+		{
+			SIM868_HTTP_TerminateSession(atTerm);
+			SIM868_HTTP_StartSession(atTerm, CONFIG_CONNECTION_ID);
+			ongoingRequest = false;
+		}
+	}
 
-void HTTP_SetBusy(bool busy) { ongoingRequest = busy; }
+	return ongoingRequest;
+}
+
+void HTTP_SetBusy(bool busy)
+{
+	if (busy)
+		requestStartTime = Time_ms();
+
+	ongoingRequest = busy;
+}
