54069d6086bdabe5f63ee573c9b7f7c81b8c05b1
Dinamiet <iceman.sm39@gmail.com>
2026-02-11 22:41:39 +0200

    Add watchdog timer

--- src/utilities/heartbeat.c
+++ src/utilities/heartbeat.c
@@ -1,11 +1,15 @@
+#include "config.h"
+#include "hardware/watchdog.h"
 #include "pico/stdlib.h"
 #include "tasks.h"
 #include "utilities.h"
 
+#include <stdio.h>
+
 #define HEARTBEAT_LED 25
 
 static SchedulerTask heartbeatTask;
-static size_t ledToggle = 0;
+static size_t        ledToggle = 0;
 
 static void heartbeat_Handler(void* _);
 
@@ -13,6 +17,7 @@ static void heartbeat_Handler(void* _)
 {
 	(void)_; // Unused
 	gpio_put(HEARTBEAT_LED, ledToggle++ & 1);
+	watchdog_update();
 }
 
 void HeartBeat_Setup()
@@ -20,6 +25,11 @@ void HeartBeat_Setup()
 	gpio_init(HEARTBEAT_LED);
 	gpio_put(HEARTBEAT_LED, false);
 	gpio_set_dir(HEARTBEAT_LED, GPIO_OUT);
+
+	if (watchdog_caused_reboot())
+		printf("Watchdog reboot\n");
+
+	watchdog_enable(CONFIG_WATCHDOG_TIMEOUT, true);
 
 	Scheduler_CreateRecurringTask(scheduler, &heartbeatTask, TASK_HEARTBEAT, heartbeat_Handler, NULL, 500);
 }
